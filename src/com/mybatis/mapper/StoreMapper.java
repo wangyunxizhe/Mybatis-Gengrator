79b64a44abffd6a36fff8480aab675119c0b36e6
wangyunxizhe <543456229@qq.com>
2019-04-01 20:57:10 +0800

    mybatis逆向工程

---
+++ src/com/mybatis/mapper/StoreMapper.java
@@ -0,0 +1,17 @@
+package com.mybatis.mapper;
+
+import com.mybatis.entity.Store;
+
+public interface StoreMapper {
+    int deleteByPrimaryKey(String storeId);
+
+    int insert(Store record);
+
+    int insertSelective(Store record);
+
+    Store selectByPrimaryKey(String storeId);
+
+    int updateByPrimaryKeySelective(Store record);
+
+    int updateByPrimaryKey(Store record);
+}
